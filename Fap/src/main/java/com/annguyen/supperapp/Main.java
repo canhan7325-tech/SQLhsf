9a43b6c3de14de01936650be084c7bc0e492362e
An Nguyen <canhan7325@gmail.com>
2025-09-14 19:18:34 +0700

    Cập nhật code sau khi chỉnh sửa

--- Fap/src/main/java/com/annguyen/supperapp/Main.java
+++ Fap/src/main/java/com/annguyen/supperapp/Main.java
@@ -9,38 +9,14 @@ import java.sql.ResultSet;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Connection conn = null;
-        try {
 
-            String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=HSF302;encrypt=true;trustServerCertificate=true";
-            String user = "sa";
-            String pass = "12345";
-            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            conn = DriverManager.getConnection(dbURL, user, pass);
-            System.out.println("Connect to DB successfully");
-            // TAOJ CLASS PREPAREDSTATEMENT dung de quan li cau sql (where, insert, update, delete)
-            PreparedStatement pstm = conn.prepareStatement("SELECT * FROM Subject");
-            ResultSet rs = pstm.executeQuery(); // thuc thi cau sql va tra ve ResultSet
-            // giong ArrayList, chua nhiu dong/record, moi dong la 1 info cua mon hoc
-            //Code|name|credit|....
-            //vong lap cay cac dong, moi dong chu dong nho ten cot table, lat cot
-            //nhuoc diem cua JDBC thuan
-            while (rs.next()) {
-                String code = rs.getString("code");
-                String name = rs.getString("name");
-                int credit = rs.getInt("credits");
-                int hour = rs.getInt("studyhours");
-                //vasghjdfaisdgaksjdkasjhdkajhsdkashdjashda
-                
-                //System.out.println(code + "\t" + name + "\t" + credit + "\t" + hour);
-                System.out.printf("|%10s|%-40s|%2d|%4d\n", code, name, credit, hour);// in gióng lề
+    }
+    //INSERT / TAO MOI SV
+    public static  void  insertStudent(){
 
-            }
-            conn.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-
+    }
+    // SELLECT * LAY HET DATA
+    public static  void  getAllStudent(){
+        
     }
 }
---
+++ Fap/src/main/java/com/annguyen/supperapp/entity/Student.java
@@ -0,0 +1,81 @@
+package com.annguyen.supperapp.entity;
+
+
+import jakarta.persistence.*;
+//
+
+// CLASS nay se dc khai de
+// anh xa/ bien doi tuong duong, dc Map thanh table tuong ung
+@Entity
+@Table(name = "Student") // neu khong co khai bao nay thi mac dinh lay ten class
+// lam ten table
+public class Student {
+    @Id
+    @Column(name = "Id") // neu khong co khai bao nay thi mac dinh lay feild lam cot
+    private  String id;// CAMEL CASE, id tu nhao (id tu tang tinh sau
+
+    @Column(name = "Name", nullable = false, length = 50)
+    private  String name;
+
+    @Column(name = "Yob", nullable = false)
+    private  int yob;
+
+    @Column(name = "Gpa", nullable = false)
+    private  double gpa;
+
+    // BAT BUOC PHAI CO CONTRUCTOR RONG, CONTRUCTOR FULL THAM SO
+    //getter, setter, toString()
+    // Boilder plate , lombok
+    // them dependency lombok vao file pom.xml
+
+    public Student() {
+    }
+    public Student(String id, String name, int yob, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.yob = yob;
+        this.gpa = gpa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYob() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = yob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", yob=" + yob +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
